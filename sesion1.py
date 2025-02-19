38de5985a0539a6c795fa6de42a77569f1fb6ebb
PC-02 <jr2025cesde>
2025-02-19 09:24:41 -0500

    first commit

---
+++ sesion1.py
@@ -0,0 +1,7 @@
+print("hoal mundo")
+print ("NOMBRE: Javier")
+print ("APELLIDO PATERNO: Rua")
+print("APELLIDO MATERNO: Moreno")
+print ("EDAD: 23 Años")
+print("CARRERA: Administracion de centros de computo")
+
